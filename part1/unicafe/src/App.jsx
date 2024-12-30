72889424ae0bcd445b4a1f90d384831bd873a4f7
JuampiConejera <juampiconejera@gmail.com>
2024-12-30 18:49:09 -0300

    commit exercise 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={handleClickGood}>Good</button>
+      <button onClick={handleClickNeutral}>Neutral</button>
+      <button onClick={handleClickBad}>Bad</button>
+      <h1>statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {total}</p>
+    </div>
+  )
+}
+
+export default App
