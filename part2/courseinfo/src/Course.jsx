d4011ad5f9dda61266cd81bc25d997820ec0e91f
JuampiConejera <juampiconejera@gmail.com>
2025-01-02 10:05:45 -0300

    commit exercise 2.2 & 2.3

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -2,10 +2,15 @@ import Header from "./Header"
 import Content from "./Content"
 
 export const Course = ({ course }) => {
+    const totalExercises = course.parts.reduce((sum, part) => 
+    sum + part.exercises, 0)
     return (
         <div>
             <Header courseName={course.name} />
-            {course.parts.map(parts => <Content key={parts.name} parts={parts} />)}
+            {course.parts.map(parts =>
+                 <Content key={parts.name} parts={parts} />
+                 )}
+            <p>Total of {totalExercises} exercises </p>
         </div>
     )
 }
